69cf5cc6c6ea74f5aca8c0c05e6ba2e7c48f8991
dionysus <dionysus@isitymarket.onmicrosoft.com>
2022-02-01 07:15:04 -0500

    DB, server (& server API endpoints, routes) fin; will + custom FE react app

---
+++ Chambre/server/routes/record.js
@@ -0,0 +1,83 @@
+const express = require('express')
+
+const recordRoutes = express.Router()
+
+const db0 = require('../db/conn')
+
+//convert id string to ObjId for _id
+const ObjectId = require('mongodb').ObjectId
+
+//get list of all records
+recordRoutes.route('/record').get(function (req, res) {
+  let db_connect = dbo.getDb('employees')
+  db_connect
+    .collection("records")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err
+      res.json(result)
+    })
+})
+
+//get single record by id
+
+recordRoutes.route('/record/:id').get(function (req, res) {
+  let db_connect = dbo.getDb()
+  let myquery = { _id: ObjectId( req.params.id )}
+  db_connect
+    .collection('records')
+    .findOne(myquery, function (err, result) {
+      if (err) throw err
+      res.json(result)
+    })
+})
+
+//create new record
+
+recordRoutes.route('/record/add').post(function (req, response) {
+  let db_connect = dbo.getDb()
+  let myobj = {
+    person_name: req.body.person_name,
+    person_position: req.body.person_position,
+    person_level: req.body.person_level,
+  }
+  db_connect.collection('records').insertOne(myobj, function (err, res) {
+    if (err) throw err;
+    response.json(res)
+  })
+})
+
+// update record by id
+
+recordRoutes.route('/update/:id').post(function (req, response) {
+  let db_connect = dbo.getDb()
+  let myquery = { _id: ObjectId( req.params.id )}
+  let newvalues = {
+    $set: {
+      person_name: req.body.person_name,
+      person_position: req.body.person_position,
+      person_level: req.body.person_level,
+    }
+  }
+  db_connect
+    .collection('records')
+    .updateOne(myquery, newvalues, function (err, res) {
+      if (err) throw err
+      console.log('1 doc updated')
+      response.json(res)
+    })
+})
+
+// delete a record
+
+recordRoutes.route('/:id').delete((req, response) => {
+  let db_connect = dbo.getDb()
+  let myquery = { _id: ObjectId( req.params.id )}
+  db_connect.collection('records').deleteOne(myquery, function (err, obj) {
+    if (err) throw err
+    console.log('1 doc deleted')
+    response.status(obj);
+  })
+})
+
+module.exports = recordRoutes;
---
+++ Chambre/server/server.js
@@ -0,0 +1,19 @@
+const express =  require('express')
+const app = express()
+const cors = require('cors')
+require ('dotenv').config({ path: './config.env'})
+const port = process.env.PORT || 5222
+app.use(cors())
+app.use(express.json())
+app.use(require('./routes/record'));
+
+// get driver connection
+const dbo = require('./db/conn')
+
+app.listen(port, () => {
+  // perform db connect on server start
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err)
+  })
+  console.log(`Server running on port: ${port}`)
+});
